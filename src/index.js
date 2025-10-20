dfcf5e1052f55261135129e560190515881b96e4
akhiljith <akhiljith.kumar.2025@mumail.ie>
2025-10-20 03:03:52 +0100

    unregister SW

--- src/index.js
+++ src/index.js
@@ -15,3 +15,9 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs =>
+    regs.forEach(reg => reg.unregister())
+  );
+}
+
